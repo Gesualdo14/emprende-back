00cf11b9b879c7f51a8b77803e5f14b69d78686a
Martín Gesualdo <mgesudev@gmail.com>
2023-05-04 18:30:30 -0300

    Pruebas

--- index.js
+++ index.js
@@ -3,23 +3,14 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT
 
-// Middleware personalizado para permitir solicitudes sólo desde el propio servidor
-const allowOnlyFromServer = (req, res, next) => {
-  const serverIPAddress = req.connection.localAddress
-  const requestIPAddress = req.connection.remoteAddress
-  console.log({ serverIPAddress, requestIPAddress })
-  if (serverIPAddress === requestIPAddress) {
-    next()
-  } else {
-    res.status(403).send("Forbidden")
-  }
-}
-
 // Servir archivos estáticos
 app.use((req, res, next) => {
   const referer = req.headers.referer
-  console.log({ method: req.method, referer })
-  next()
+  if (referer !== "https://emprende-back.onrender.com/") {
+    res.status(403).send("Forbidden")
+  } else {
+    next()
+  }
 })
 app.use(express.static("public"))
 
@@ -28,7 +19,7 @@ app.get("/", function (req, res) {
   res.send("Hello World!")
 })
 
-app.get("/users", allowOnlyFromServer, function (req, res) {
+app.get("/users", function (req, res) {
   res.send([{ name: "Martin" }, { name: "Francisco" }])
 })
 
