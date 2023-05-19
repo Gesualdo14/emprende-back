ac01bf30605b73e34a6e581c1924cc3c459ef932
Martín Gesualdo <mgesudev@gmail.com>
2023-05-19 07:59:15 -0300

    Cambios

--- index.js
+++ index.js
@@ -2,25 +2,96 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const port = process.env.PORT
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-// Servir archivos estáticos
-app.use((req, res, next) => {
-  const referer = req.headers.referer
-  console.log({ origin: req.origin, referer })
-  next()
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Conexión exitosa con la BBDD!")
+  })
+  .catch((err) =>
+    console.log("Hubo un error al conectarnos a la BBDD", { err })
+  )
+
+const taskSchema = new Schema({
+  name: String,
+  done: Boolean,
+  // createdBy:
 })
+
+const Task = mongoose.model("Task", taskSchema, "Tasks")
+
+// Middlewares
 app.use(express.static("public"))
+app.use(express.json())
 
-// COnfigurar RUTAS
-app.get("/", function (req, res) {
-  res.send("Hello World!")
+// Configurar RUTAS
+app.get("/api/tasks", function (req, res) {
+  Task.find()
+    .then((tasks) => {
+      res.status(200).json({ ok: true, data: tasks })
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .json({ ok: true, message: "Hubo un problema al obtener las tareas" })
+    })
 })
 
-app.get("/users", function (req, res) {
-  res.send([{ name: "Martin" }, { name: "Francisco" }])
+app.post("/api/tasks", function (req, res) {
+  const body = req.body
+  console.log({ body })
+  Task.create({
+    name: body.text,
+    done: false,
+    hello: "HOLA",
+  })
+    .then((createdTask) => {
+      res.status(201).json({
+        ok: true,
+        message: "Tarea creada con éxito",
+        data: createdTask,
+      })
+    })
+    .catch((err) => {
+      res.status(400).json({ ok: false, message: "Error al crear la tarea" })
+    })
+})
+
+app.put("/api/tasks/:id", function (req, res) {
+  const body = req.body
+  const id = req.params.id
+
+  Task.findByIdAndUpdate(id, {
+    name: body.text,
+  })
+    .then((updatedTask) => {
+      res.status(200).json({
+        ok: true,
+        message: "Tarea editada con éxito",
+        data: updatedTask,
+      })
+    })
+    .catch((err) => {
+      res.status(400).json({ ok: false, message: "Error al editar la tarea" })
+    })
+})
+
+app.delete("/api/tasks/:id", function (req, res) {
+  const id = req.params.id
+  Task.findByIdAndRemove(id)
+    .then((deletedTask) => {
+      res.status(200).json({ ok: true, data: deletedTask })
+    })
+    .catch(() => {
+      res
+        .status(400)
+        .json({ ok: false, message: "Hubo un error al eliminar la tarea" })
+    })
 })
 
 // Poner a escuchar la APP en un puerto
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}. Dirname: ${__dirname}`)
+  console.log(`App listening on port: ${port}`)
 })
